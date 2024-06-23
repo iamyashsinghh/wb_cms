c84d7081682104a28a624a215dc865369ad29d14
Yash Singh <iamyashsinghh@gmail.com>
2024-06-24 00:29:35 +0530

    ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends Controller
             Mail::to($user->email)->send(new OtpMail($otp, $user));
         }
 
-        $this->sendWhatsAppMessage($user->phone,$user->name, $user-> $otp);
+        $this->sendWhatsAppMessage($user->phone,$user->name, $user->$otp);
         return response()->json(['success' => true, 'alert_type' => 'success', 'message' => 'Verification code has been sent to your registered WhatsApp & Email.'], 200);
     }
 
@@ -108,7 +108,7 @@ class AuthController extends Controller
         return redirect()->route('login');
     }
 
-    private function sendWhatsAppMessage($phone,$name, $otp)
+    private function sendWhatsAppMessage($phone, $name, $otp)
     {
         if (env('TATA_WHATSAPP_MSG_STATUS') !== true) {
             return false;
