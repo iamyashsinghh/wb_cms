51c380f1be0b41adf8bf11740274ba2e003f66fd
Yash Singh <iamyashsinghh@gmail.com>
2024-06-26 10:39:02 +0530

    ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,7 +50,7 @@ class AuthController extends Controller
         );
 
         if($user->email){
-            // Mail::to($user->email)->send(new OtpMail($otp, $user));
+            Mail::to($user->email)->send(new OtpMail($otp, $user));
         }
 
         $this->sendWhatsAppMessage($user->phone,$user->name,$otp);
