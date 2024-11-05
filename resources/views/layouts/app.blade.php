7a533a7a86e3d9301fa3a057fa1b6e64fe09986a
Yash Singh <iamyashsinghh@gmail.com>
2024-11-05 12:16:03 +0530

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
 <body class="sidebar-mini layout-fixed">
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="" src="{{ asset('happy-diwali-diwali.png') }}" alt="AdminLTELogo" style="width: 30%; height: 50%;">
+        <img class="" src="{{ asset('wb-logo2.webp') }}" alt="AdminLTELogo" style="width: 20%; height: 10%;">
         {{-- class="animation__shake" --}}
     </div>
     @include('layouts.navbar')
